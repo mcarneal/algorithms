edc03a721f627780686eadeb38c1831fd4ee6b90
Michael Carneal <mcarneal09@yahoo.com>
2020-08-26 13:47:56 -0400

    added jest testing framework

---
+++ _tests_/firstTest.spec.js
@@ -0,0 +1,14 @@
+// import {describe, test} from "@jest/globals";
+const {
+    describe,
+    test,
+} = require(`@jest/globals`)
+
+describe("Filter function", () => {
+    test("it should filter by a search term (link)", () => {
+        expect(1).toEqual(1);
+    });
+    test("it should pass", () => {
+        expect(1).toEqual(1);
+    });
+});
